f7aff93aac4e216a00c7d1d218afb552b8633c75
Pierre Gergondet <pierre.gergondet@gmail.com>
2010-11-18 15:55:04 +0900

    squarefunction test

---
+++ test/testsquare-function.cpp
@@ -0,0 +1,27 @@
+#include <ssvp-interface/square-function.h>
+
+#include <iostream>
+
+using namespace ssvpinterface;
+
+int main(int argc, char * argv[])
+{
+    int frequency = 1;
+    int screenFrequency = 60;
+    if(argc > 2)
+    {
+        frequency = atoi(argv[1]);
+        screenFrequency = atoi(argv[2]);
+    }
+    std::vector< std::pair<int,int> > seq;
+    if(squarefunction(frequency, screenFrequency, seq))
+    {
+        std::cout << " [ ";
+        for(int i = 0; i < seq.size(); ++i)
+        {
+            std::cout << " < " << seq[i].first << " , " << seq[i].second << " > ";
+        }
+        std::cout << " ] " << std::endl;
+    }
+    return 0;
+}
