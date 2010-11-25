486d6d646e8853ae0d848c5a85e5e54759931fdb
Pierre Gergondet <pierre.gergondet@gmail.com>
2010-11-25 19:33:52 +0900

    Config file implementation

--- test/testssvp-interface.cpp
+++ test/testssvp-interface.cpp
@@ -1,4 +1,5 @@
 #include <ssvp-interface/SSVPInterface.h>
+#include <ssvp-interface/SSVPInterfaceConfig.h>
 
 #include <SFML/Graphics.hpp>
 
@@ -20,47 +21,39 @@ using namespace ssvpinterface;
 
 SSVPInterface * interface = 0;
 
-void otherLoop()
-{
-    for(int timer = 0; timer < 15; ++timer)
-    {
-        std::cout << timer << std::endl;
-        if(timer == 2)
-        {
-            interface->EnableCross(true);
-        }
-        if(timer >= 2)
-        {
-            interface->EnableArrow(timer - 2);
-        }
-        if(timer == 7)
-        {
-            interface->EnableFlash(false);
-        }
-        if(timer == 10)
-        {
-            interface->EnableFlash(true);
-        }
-        sleep(1);
-    }
-    if(interface)
-    {
-        interface->Close();
-    }   
-}
-
 int main(int argc, char * argv[])
 {
-    int winW = 1280;
-    int winH = 800;
-    interface = new SSVPInterface(winW, winH);
+    bool fullscreen = true;
+    if(argc > 1)
+    {
+        std::string configName = argv[1];
+        SSVPInterfaceConfig config;
+        config.ReadFromFile(configName);
 
-    interface->AddSquare(new FlickeringSquare(5,60, winW/2-75, 50, DOWN, 150, 255, 0, 0));
-    interface->AddSquare(new FlickeringSquare(7,60, winW-350, winH/2-50, LEFT, 150, 255, 0, 0));
-    interface->AddSquare(new FlickeringSquare(9,60, winW/2-75, winH-200, UP, 150, 255, 0, 0));
-    interface->AddSquare(new FlickeringSquare(11,60, 200, winH/2-50, RIGHT, 150, 255, 0, 0));
+        interface = new SSVPInterface(config.GetWidth(), config.GetHeight());
 
-    boost::thread th(&SSVPInterface::DisplayLoop, interface, false);
+        fullscreen = config.IsFullScreen();
+
+        for(unsigned int i = 0; i < config.GetSquares().size(); ++i)
+        {
+            interface->AddSquare(config.GetSquares()[i]);
+        }
+
+    }
+    else
+    {
+        int winW = 1280;
+        int winH = 800;
+
+        interface = new SSVPInterface(winW, winH);
+
+        interface->AddSquare(new FlickeringSquare(5,60, winW/2-75, 50, DOWN, 150, 255, 0, 0, 255, false));
+        interface->AddSquare(new FlickeringSquare(7,60, winW-350, winH/2-50, LEFT, 150, 255, 0, 0, 255, false));
+        interface->AddSquare(new FlickeringSquare(9,60, winW/2-75, winH-200, UP, 150, 255, 0, 0, 255, false));
+        interface->AddSquare(new FlickeringSquare(11,60, 200, winH/2-50, RIGHT, 150, 255, 0, 0, 255, false));
+    }
+
+    boost::thread th(&SSVPInterface::DisplayLoop, interface, fullscreen);
     
     th.join();
 
