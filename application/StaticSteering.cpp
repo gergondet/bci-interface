67fcc4f9929c81738de12001edff37fe498c17ae
Pierre Gergondet <pierre.gergondet@gmail.com>
2011-06-07 16:43:06 +0900

    Add a static cross steering application for HRP-2

---
+++ application/StaticSteering.cpp
@@ -0,0 +1,63 @@
+#include <bci-interface/BCIInterface.h>
+#include <bci-interface/VisionServerBS.h>
+#include <bci-interface/SSVEPStimulus.h>
+#include <bci-interface/UDPReceiver.h>
+#include <bci-interface/CoshellInterpreter.h>
+
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+#include <sstream>
+
+#ifdef WIN32
+#include <Windows.h>
+void sleep(DWORD t)
+{
+	Sleep(1000*t);
+}
+#endif
+
+using namespace bciinterface;
+
+int main(int argc, char * argv[])
+{
+    bool fullscreen = false;
+    unsigned int width = 1280;
+    unsigned int height = 800;
+
+    BCIInterface * bciinterface = new BCIInterface(width, height);
+
+    UDPReceiver * receiver = new UDPReceiver(1111);
+    bciinterface->SetCommandReceiver(receiver);
+
+    CoshellInterpreter * interpreter = new CoshellInterpreter("localhost", 2809);
+    std::vector<std::string> commands;
+    commands.push_back("set pg.velocitydes [3](0.0001,0.0,0.0)");
+    commands.push_back("set pg.velocitydes [3](0.20,0.0,0.0)");
+    commands.push_back("set pg.velocitydes [3](0.0001,0.0,-0.2)");
+    commands.push_back("set pg.velocitydes [3](-0.20,0.0,0.0)");
+    commands.push_back("set pg.velocitydes [3](0.0001,0.0,0.2)");
+    interpreter->SetCommands(commands);
+    std::string initialtest = "pg.velocitydes";
+    interpreter->SetInitialTest(initialtest);
+    std::vector<std::string> initialcommands;
+    initialcommands.push_back("import walking/startherdt");
+    initialcommands.push_back("set pg.velocitydes [3](0.0001,0.0,0.0)");
+    interpreter->SetInitialCommands(initialcommands);
+    bciinterface->SetCommandInterpreter(interpreter);
+
+    bciinterface->SetBackgroundSprite(new VisionServerBS("localhost", 4242, 640, 480));
+    
+    bciinterface->AddObject(new SSVEPStimulus(7,60, width/2-75, 50, 150, 150, 255, 0, 0, 255));
+    bciinterface->AddObject(new SSVEPStimulus(12,60, width-225, height/2-75, 150, 150, 255, 0, 0, 255));
+    bciinterface->AddObject(new SSVEPStimulus(5,60, width/2-75, height-200, 150, 150, 255, 0, 0, 255));
+    bciinterface->AddObject(new SSVEPStimulus(9,60, 75, height/2-75, 150, 150, 255, 0, 0, 255));
+
+    bciinterface->DisplayLoop(fullscreen);
+
+    delete bciinterface;
+    delete interpreter;
+    delete receiver;
+
+    return 0;
+}
