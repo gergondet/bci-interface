389321374792009cc76eb451b38fb7c7e5e01e14
Pierre Gergondet <pierre.gergondet@aist.go.jp>
2012-01-05 18:41:23 +0900

    Add a simple TextObject to display text on the screen

---
+++ include/bci-interface/DisplayObject/TextObject.h
@@ -0,0 +1,31 @@
+#ifndef _H_TEXTOBJECT_H_
+#define _H_TEXTOBJECT_H_
+
+#include <bci-interface/DisplayObject.h>
+
+#include <boost/shared_ptr.hpp>
+
+namespace bciinterface
+{
+
+struct TextObjectImpl;
+
+class TextObject : public DisplayObject
+{
+public:
+    TextObject(const std::string & txt);
+
+    void Display(sf::RenderWindow * app, unsigned int frameCount, sf::Clock & clock);
+
+    void SetPosition(float x, float y);
+
+    void SetText(const std::string & txt);
+
+    void SetCharacterSize(unsigned int size);
+private:
+    boost::shared_ptr<TextObjectImpl> m_impl;
+};
+
+}  // namespace bciinterface
+
+#endif
