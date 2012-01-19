1c9ac3097e391f59a68c277540914ce4c00b647a
Pierre Gergondet <pierre.gergondet@aist.go.jp>
2012-01-19 14:24:37 +0900

    Implements new StaticBG class

---
+++ include/bci-interface/Background/StaticBG.h
@@ -0,0 +1,39 @@
+/* A BackgroundSprite implementation with a static image */
+
+#include <bci-interface/Background.h>
+
+#include <boost/shared_ptr.hpp>
+#include <string>
+
+#ifndef _H_STATICBG_H_
+#define _H_STATICBG_H_
+
+namespace bciinterface
+{
+
+struct StaticBGImpl;
+
+class StaticBG : public Background
+{
+public:
+    StaticBG(const std::string & src_file, 
+        unsigned int wwidth, unsigned int wheight, unsigned int iwidth = 0, unsigned int iheight = 0);
+
+    void UpdateLoop();
+
+    void Close();
+
+    void Draw(sf::RenderWindow * app);
+
+    void ChangeImage(const std::string & new_src);
+
+    void SetSubRect(int left, int top, int width, int height);
+
+private:
+    boost::shared_ptr<StaticBGImpl> m_impl;
+};
+
+}
+
+#endif
+
