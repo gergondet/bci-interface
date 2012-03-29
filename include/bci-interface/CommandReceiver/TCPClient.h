87d0d5b6aee672eae1b96b11e21d15b8f14c2167
Pierre Gergondet <pierre.gergondet@aist.go.jp>
2012-03-29 14:11:45 +0900

    Add TCPClient for CommandReceiver

---
+++ include/bci-interface/CommandReceiver/TCPClient.h
@@ -0,0 +1,41 @@
+/* Receive commands through UDP connection*/
+
+#ifndef _H_TCPCLIENT_H_
+#define _H_TCPCLIENT_H_
+
+#include <bci-interface/CommandReceiver.h>
+
+#include <boost/shared_ptr.hpp>
+
+namespace bciinterface
+{
+
+struct TCPClientImpl;
+
+class TCPClient : public CommandReceiver
+{
+public:
+    TCPClient(const std::string & srv_name, unsigned short port);
+
+    virtual ~TCPClient() {}
+
+    /* Loop to receive command */
+    virtual void CommandLoop();
+
+    /* Close command Client */
+    virtual void Close();
+
+    /* Returns the current command output from the BCI */
+    virtual int GetCommand();
+
+    /* Sends a message to the server */
+    void SendMessage(const std::string & message);
+
+private:
+    boost::shared_ptr<TCPClientImpl> m_impl;
+};
+
+} // namespace bciinterface
+
+#endif
+
