def4e125b2e6e7975bfd73af037c5b2caccacc2c
Pierre Gergondet <pierre.gergondet@aist.go.jp>
2013-09-15 15:07:12 +0900

    Some more OpenGL settings

--- src/BCIInterface.cpp
+++ src/BCIInterface.cpp
@@ -117,6 +117,10 @@ public:
 
         /* Position the camera */
         glTranslatef(0, 0, 0);
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LEQUAL);
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
     }
 
     void Resize()
